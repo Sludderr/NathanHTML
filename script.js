8a4d606a5f1b5822a9385b4ca6ba9f1fd95f6578
Sludderr <108181849+Sludderr@users.noreply.github.com>
2022-12-26 20:54:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let size = 0;
+let size = 10;
+let positionX = 500;
+let positionY = 100;
+let velocityX = 0;
+let velocityY = 0;
 
-function animate(){
-    size ++;
+function drawCircle(){
     ctx.fillStyle = "black";
     ctx.lineWidth = 5;
     ctx.beginPath();
-    ctx.arc(100, 100, size, 0, Math.PI * 2);
+    ctx.arc(positionX, positionY, size, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
+}
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    velocityY += 0.1633;
+    positionX += velocityX;
+    positionY += velocityY;
+    drawCircle();
     requestAnimationFrame(animate);
 }
 animate();
